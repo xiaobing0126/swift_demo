64dccdd5f4db91bdbc992bcf1504423e49cb59cd
zongbinglin <804968295@qq.com>
2026-02-27 10:23:37 +0800

    初始化swift_demo项目

---
+++ swift_demo/swift_demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_demo
+//
+//  Created by 小饼子 on 2026/2/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_demo/swift_demo/swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_demoApp.swift
+//  swift_demo
+//
+//  Created by 小饼子 on 2026/2/27.
+//
+
+import SwiftUI
+
+@main
+struct swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
